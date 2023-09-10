c2609a8f2f21336e1551a3919779fe4fb5567f57
Maximilian <scientificresearch309@gmail.com>
2023-09-10 17:13:25 +0200

    model was defined and find all, sort according to the name and select only the author and name

--- index.js
+++ index.js
@@ -8,16 +8,25 @@ mongoose
 
 // define Schema
 const courseSchema = new mongoose.Schema({
-    name: String,
-    author: String,
-    tags: [String],
-    date: {
-      type: Date,
-      default: Date.now,
-    },
-    isPublished: Boolean,
-    price: Number,
-  });
-  
-  // define Model
+  name: String,
+  author: String,
+  tags: [String],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  isPublished: Boolean,
+  price: Number,
+});
 
+// define Model
+const Course = mongoose.model("Course", courseSchema);
+
+const getCourses = async () => {
+  const courses = await Course.find()
+    .sort({ name: 1 })
+    .select({ name: 1, author: 1 });
+  console.log(courses);
+};
+
+getCourses();
