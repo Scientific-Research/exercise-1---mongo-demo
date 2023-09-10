19f73df221e6fde45588093fc59fe37e7f7855c2
Maximilian <scientificresearch309@gmail.com>
2023-09-10 17:00:13 +0200

    Schema was defined!

--- index.js
+++ index.js
@@ -6,5 +6,18 @@ mongoose
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.log("Could not connect to MongoDB...", err));
 
-
+// define Schema
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+    isPublished: Boolean,
+    price: Number,
+  });
+  
+  // define Model
 
